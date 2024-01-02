39d071d2dc5ed96a7164c4b41b3a8bdace0f2e52
ness816 <ness41861@gmail.com>
2024-01-02 17:56:32 +0700

    ยากจุง

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,55 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N ,double B[])
+{
+    double AM , SD , GM ,H;
+    double sum = 0 , sumsquare = 0,gm = 1, ha = 0,MAX = A[0],MIN = A[0];
+    
+    int i = 0;
+    
+    while (i < N)
+    {
+        sum += A[i];
+        sumsquare += pow(A[i],2);
+        gm *= A[i];
+        ha += 1.00/A[i];
+        
+        if(MAX > A[i])
+        {
+           //ใช้ค่าMAXค่าเดิม
+        }
+        else
+        {
+            MAX = A[i];
+
+        }
+
+         if(MIN < A[i])
+        {
+            //ใช้ค่าMINค่าเดิม
+        }
+        else
+        {
+            MIN = A[i];
+
+        }
+
+        i++;
+    }
+    AM = sum/N ;
+    SD =sqrt((sumsquare/N)-(pow(AM,2)));
+    GM = pow(gm,1.00/N);
+    H = N/ha;
+   
+ 
+
+    B[0] =AM;
+    B[1] = SD;
+    B[2] = GM;
+    B[3] = H;
+    B[4] = MAX;
+    B[5] = MIN;
+
+}
